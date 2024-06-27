405ea9386620fd8ee908c9ce177754fa1a4afa6f
Maria Natália <cardim.natalia@gmail.com>
2024-06-26 21:31:40 -0300

    checagem se email existe no cadastro

--- php/processa_cadastro.php
+++ php/processa_cadastro.php
@@ -5,17 +5,22 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// Criptografar a senha
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+//Verifica se o email existe
+$emailExiste = 0;
+$emailExiste = mysqli_query($conexao, "SELECT * FROM user WHERE email = '$email'");
 
-// Inserir os dados no banco de dados
-$query = mysqli_query($conexao, "INSERT INTO user(nome, email, senha) VALUES ('$nome', '$email', '$hashed_password')");
+if(mysqli_num_rows($emailExiste) == 0){
+    // Criptografar a senha
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-if ($query) {
+    // Inserir os dados no banco de dados
+    $query = mysqli_query($conexao, "INSERT INTO user(nome, email, senha) VALUES ('$nome', '$email', '$hashed_password')");
+
     // Redirecionar para a página de sucesso
     header("Location: ../pages/login.html");
     exit();
 } else {
     echo "Erro ao cadastrar o usuário.";
+    echo "Email já cadastrado.";
 }
 ?>
